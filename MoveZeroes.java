7db890349b2c09ea8b79332ba2d4aaab3335e4aa
ss.santhosh <147931093+santhosh1282004@users.noreply.github.com>
2025-04-25 21:53:17 +0530

    Create MoveZeroes.java

---
+++ MoveZeroes.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int ind=0;
+        int len=nums.length;
+        if(len==1) return ;
+        for(int i=0;i<len;i++){
+            if(nums[i]!=0){
+                nums[ind]=nums[i];
+                ind++;
+            }
+        }
+        for(;ind<len;ind++)nums[ind]=0;
+        return ;
+    }
+}
